8605ad8eddd6ea17e8fda509268632123edd7831
Ajinkya Bagde <ajinkya.bagde@workday.com>
2021-02-03 23:38:15 -0800

    Almost done with dt stuff

--- DTLearner.py
+++ DTLearner.py
@@ -31,10 +31,10 @@ class DTLearner(object):
         self.verbose = verbose
 
         # NOTE: Add alpha to param_dict, figure out wtf it is
-        self.param_dict = {"criterion": ['gini','entropy'], "max_depth": range(1,3), "min_samples_split": range(2,4), "min_samples_leaf": range(1,4)}
+        self.param_dict = {"criterion": ['gini','entropy'], "max_depth": range(1,10), "min_samples_split": range(2,10), "min_samples_leaf": range(1,5)}
         self.grid = 0
 
-    def train(self, X_train, y_train):
+    def train(self, X_train, y_train, flag):
         '''
 
         :param X_train: training data
@@ -45,36 +45,38 @@ class DTLearner(object):
         if self.verbose:
             print("Training Decision Tree Model...")
 
-        path = self.clf.cost_complexity_pruning_path(X_train, y_train)
-        ccp_alphas, impurities = path.ccp_alphas, path.impurities
-        ccp_alphas = ccp_alphas[0::20]
-        impurities = impurities[0::20]
-        #print(ccp_alphas)
-        #self.param_dict['ccp_alpha'] = ccp_alphas
+        if flag == 0:
 
-        # self.grid = GridSearchCV(self.clf, param_grid = self.param_dict, cv=self.n_folds, verbose=1, n_jobs=-1)
-        # self.grid.fit(X_train, y_train)
+            # Use cost_complexity_pruning_path to get effective alphas (these is just what values of ccp_alpha could be appropriate)
+            # We can also get corresponding leaf impurities if desired (not needed for now)
+            # https://scikit-learn.org/stable/auto_examples/tree/plot_cost_complexity_pruning.html
+            path = self.clf.cost_complexity_pruning_path(X_train, y_train)
+            ccp_alphas, impurities = path.ccp_alphas, path.impurities
+            ccp_alphas = ccp_alphas[0::5]
 
-        # print(self.grid.best_params_)
+            clfs = []
+            for ccp_alpha in ccp_alphas:
+                clf = DecisionTreeClassifier(random_state=0, ccp_alpha=ccp_alpha)
+                clf.fit(X_train, y_train)
+                clfs.append(clf)
+            # print("Number of nodes in the last tree is: {} with ccp_alpha: {}".format(
+            #     clfs[-1].tree_.node_count, ccp_alphas[-1]))
+            clfs = clfs[:-1]
+            ccp_alphas = ccp_alphas[:-1]
 
-        # Total impurity of leaves vs effective alphas of pruned tree
-        # fig, ax = plt.subplots()
-        # ax.plot(ccp_alphas[:-1], impurities[:-1], marker='o', drawstyle="steps-post")
-        # ax.set_xlabel("effective alpha")
-        # ax.set_ylabel("total impurity of leaves")
-        # ax.set_title("Total Impurity vs effective alpha for training set")
+            return clfs, ccp_alphas
 
-        # Number of Nodes vs alpha and Depth vs Alpha
-        clfs = []
-        for ccp_alpha in ccp_alphas:
-            clf = DecisionTreeClassifier(random_state=0, ccp_alpha=ccp_alpha)
-            clf.fit(X_train, y_train)
-            clfs.append(clf)
-        print("Number of nodes in the last tree is: {} with ccp_alpha: {}".format(
-            clfs[-1].tree_.node_count, ccp_alphas[-1]))
+        if flag == 1:
 
-        clfs = clfs[:-1]
-        ccp_alphas = ccp_alphas[:-1]
+            possible_depths = [1,2,3,4,5,6,7,8,9]
+
+            clfs = []
+            for depth in possible_depths:
+                clf = DecisionTreeClassifier(random_state=0, max_depth=depth)
+                clf.fit(X_train, y_train)
+                clfs.append(clf)
+
+            return clfs, possible_depths
 
         # node_counts = [clf.tree_.node_count for clf in clfs]
         # depth = [clf.tree_.max_depth for clf in clfs]
@@ -90,12 +92,9 @@ class DTLearner(object):
         # fig.tight_layout()
         # plt.show()
 
-        return clfs, ccp_alphas
 
 
-
-
-    def test(self, X_test,X_train, y_test, y_train, clfs, alphas):
+    def test(self, X_test,X_train, y_test, y_train, clfs, alphas, depths, flag):
         '''
 
         :param X_test: test data
@@ -106,27 +105,62 @@ class DTLearner(object):
         if self.verbose:
             print("Testing Decision Tree Model...")
 
-        self.accuracy_score_train = []
-        self.accuracy_score_test = []
+        if flag == 0:
+            self.accuracy_score_train = []
+            self.accuracy_score_test = []
 
-        for clf in clfs:
-            predictions_train = clf.predict(X_train)
-            predictions_test = clf.predict(X_test)
+            for clf in clfs:
+                predictions_train = clf.predict(X_train)
+                predictions_test = clf.predict(X_test)
 
-            self.accuracy_score_train.append(accuracy_score(y_train, predictions_train))
-            self.accuracy_score_test.append(accuracy_score(y_test, predictions_test))
-
-        for i in range(0, len(alphas)):
-            print("Alpha Value: ", alphas[i])
-            print("Train Accuracy:", self.accuracy_score_train[i])
-            print("Test Accuracy:", self.accuracy_score_test[i])
-
-        plt.plot(alphas, self.accuracy_score_train, label = 'Accuracy Score (Training Set)')
-        plt.plot(alphas, self.accuracy_score_test, label = 'Accuracy Score (Test Set)')
-        plt.xlabel('Alpha')
-        plt.ylabel('Accuracy')
-        plt.title('Accuracy vs Alpha Value')
-        plt.legend()
-        plt.show()
+                self.accuracy_score_train.append(accuracy_score(y_train, predictions_train))
+                self.accuracy_score_test.append(accuracy_score(y_test, predictions_test))
 
 
+            # Print out best Accuracy/Alpha combination
+            print("Best Accuracy Score (Test Validation Set): ", max(self.accuracy_score_test))
+            print("Best Alpha (Highest Accuracy, Test Validation Set): ", alphas[self.accuracy_score_test.index(max(self.accuracy_score_test))])
+
+            plt.figure()
+            plt.plot(alphas, self.accuracy_score_train, label = 'Accuracy Score (Training Validation Set)')
+            plt.plot(alphas, self.accuracy_score_test, label = 'Accuracy Score (Test Validation Set)')
+            plt.xlabel('Alpha')
+            plt.ylabel('Accuracy')
+            plt.title('Accuracy vs Alpha Value')
+            plt.legend()
+            plt.savefig('/Users/ajinkya.bagde/Desktop/AS1_Figs/DT/alpha_vs_accuracy.png')
+
+            return clfs[self.accuracy_score_test.index(max(self.accuracy_score_test))]
+
+        if flag == 1:
+            self.accuracy_score_train = []
+            self.accuracy_score_test = []
+
+            for clf in clfs:
+                predictions_train = clf.predict(X_train)
+                predictions_test = clf.predict(X_test)
+
+                self.accuracy_score_train.append(accuracy_score(y_train, predictions_train))
+                self.accuracy_score_test.append(accuracy_score(y_test, predictions_test))
+
+
+            # Print out best Accuracy/Depth combination
+            print("Best Accuracy Score (Test Validation Set): ", max(self.accuracy_score_test))
+            print("Best Depth (Highest Accuracy, Test Validation Set): ", depths[self.accuracy_score_test.index(max(self.accuracy_score_test))])
+
+            plt.figure()
+            plt.plot(depths, self.accuracy_score_train, label = 'Accuracy Score (Training Validation Set)')
+            plt.plot(depths, self.accuracy_score_test, label = 'Accuracy Score (Test Validation Set)')
+            plt.xlabel('Depth')
+            plt.ylabel('Accuracy')
+            plt.title('Accuracy vs Depth Value')
+            plt.legend()
+            plt.savefig('/Users/ajinkya.bagde/Desktop/AS1_Figs/DT/depth_vs_accuracy.png')
+
+            return clfs[self.accuracy_score_test.index(max(self.accuracy_score_test))]
+
+    def tune_hyperparameters(self, final_dt, xtrain, ytrain):
+        self.grid = GridSearchCV(final_dt, param_grid = self.param_dict, cv=self.n_folds, verbose=1, n_jobs=-1)
+        self.grid.fit(xtrain, ytrain)
+
+        print(self.grid.best_params_)
--- main.py
+++ main.py
@@ -12,26 +12,58 @@ from IPython.display import Image
 import pydotplus
 import graphviz
 from sklearn import tree
+import numpy as np
 
 import DTLearner as dt
 
 def test_code():
     
 
-
     # Fetch and split Data here
     mnist_data = fetch_openml('mnist_784')
     #print(mnist_data.keys())
-    X, y = mnist_data['data'], mnist_data['target']
-    X = X[0::10]
-    y = y[0::10]
+    X_whole, y_whole = mnist_data['data'], mnist_data['target']
+
+    # Take a subset of the data (10%)
+    X = X_whole[0::10]
+    y = y_whole[0::10]
+
+    # Lets validate this data (we want to see that the 10% subset is still representative of the actual data)
+    fig, ax = plt.subplots(2)
+    whole_cats, whole_counts = np.unique(y_whole, return_counts=True)
+    subset_cats, subset_counts = np.unique(y, return_counts=True)
+    ax[0].bar(whole_cats, whole_counts, label = 'Class Distribution (Entire Set)')
+    ax[1].bar(subset_cats, subset_counts, label = 'Class Distribution (Subset)')
+    plt.savefig('/Users/ajinkya.bagde/Desktop/AS1_Figs/DT/class_distribution.png')
+
+    # Split the initial data
     xtrain , xtest ,ytrain, ytest = train_test_split(X,y,test_size =0.2,shuffle = False,random_state =7)
 
     start=datetime.now()
 
-    dtlearner = dt.DTLearner(leaf_size=1, n_folds=2, verbose=True)  
-    clfs, alphas = dtlearner.train(xtrain,ytrain)
-    dtlearner.test(xtest,xtrain,ytest,ytrain,clfs,alphas)
+    ### DTLearner Implementation ###
+    dtlearner = dt.DTLearner(leaf_size=1, n_folds=3, verbose=True)  
+
+    # Create a validation set - do another train/test split on the training data
+    xtrain_val , xtest_val ,ytrain_val, ytest_val = train_test_split(X,y,test_size =0.2,shuffle = False,random_state =7)
+
+    # Get a list of possible decision trees and their respective alphas
+    flag = 0
+    clfs, alphas = dtlearner.train(xtrain_val,ytrain_val,flag)
+    # Get the decision tree that is correlated to the alpha with highest accuracy
+    depths = "NA"
+    dt_choice = dtlearner.test(xtest_val,xtrain_val,ytest_val,ytrain_val,clfs,alphas,depths,flag)
+
+    # Get a list of possible decision trees and their respective depths
+    flag = 1
+    clfs, depths = dtlearner.train(xtrain_val,ytrain_val,flag)
+    # Get the decision tree that is correlated to the depth with highest accuracy
+    alphas = "NA"
+    dt_choice = dtlearner.test(xtest_val,xtrain_val,ytest_val,ytrain_val,clfs,alphas,depths,flag)
+
+    # # Now that we have the decision tree, time for tuning hyperparameters
+    # dtlearner.tune_hyperparameters(dt_choice, xtrain, ytrain)
+
 
     print(datetime.now()-start)
 
